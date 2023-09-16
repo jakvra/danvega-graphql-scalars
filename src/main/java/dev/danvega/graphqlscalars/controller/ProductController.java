dd0106b7fd259662e18a8454b87b683841a01164
Jakub Vrana <vrana.jakub@feg.eu>
2023-09-16 23:51:32 +0200

    lombok added

--- src/main/java/dev/danvega/graphqlscalars/controller/ProductController.java
+++ src/main/java/dev/danvega/graphqlscalars/controller/ProductController.java
@@ -2,19 +2,17 @@ package dev.danvega.graphqlscalars.controller;
 
 import dev.danvega.graphqlscalars.model.Product;
 import dev.danvega.graphqlscalars.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductController {
 
     private final ProductRepository repository;
-
-    public ProductController(ProductRepository repository) {
-        this.repository = repository;
-    }
 
     @QueryMapping
     public List<Product> allProducts() {
--- src/main/java/dev/danvega/graphqlscalars/model/Product.java
+++ src/main/java/dev/danvega/graphqlscalars/model/Product.java
@@ -3,11 +3,17 @@ package dev.danvega.graphqlscalars.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Product {
 
     @Id
@@ -19,62 +25,11 @@ public class Product {
     private BigDecimal price;
     private LocalDateTime dateCreated;
 
-    public Product() {
-    }
-
     public Product(String title, Boolean isOnSale, Float weight, BigDecimal price, LocalDateTime dateCreated) {
         this.title = title;
         this.isOnSale = isOnSale;
         this.weight = weight;
         this.price = price;
-        this.dateCreated = dateCreated;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Boolean getOnSale() {
-        return isOnSale;
-    }
-
-    public void setOnSale(Boolean onSale) {
-        isOnSale = onSale;
-    }
-
-    public Float getWeight() {
-        return weight;
-    }
-
-    public void setWeight(Float weight) {
-        this.weight = weight;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public LocalDateTime getDateCreated() {
-        return dateCreated;
-    }
-
-    public void setDateCreated(LocalDateTime dateCreated) {
         this.dateCreated = dateCreated;
     }
 
